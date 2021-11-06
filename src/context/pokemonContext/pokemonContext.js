36542fd827e66ce0a239d4b6d1b202907770e7c7
David Orozco <dorozco@convene.com>
2021-11-06 00:50:26 -0500

    feat: add context to handle pokemon states

---
+++ src/context/pokemonContext/pokemonContext.js
@@ -0,0 +1,67 @@
+import {createContext, useContext, useEffect, useState} from 'react';
+import pokemonApi from '../../services/pokemonService';
+import PropTypes from 'prop-types';
+
+const PokemonContext = createContext();
+
+const PokemonProvider = ({ children }) => {
+    const [tempPokemonList, setTempPokemonList] = useState([]);
+    const [pokemonList, setPokemonList] = useState([]);
+    const [currentUrl, setCurrentUrl] = useState('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=150');
+    const [currentPokemon, setCurrentPokemon] = useState(undefined);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            // TODO: If data is in localstorage, load from there
+            const { results, next } = await pokemonApi.doGet('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=150');
+            setPokemonList(results);
+            setTempPokemonList(results);
+            setCurrentUrl(next);
+        };
+
+        fetchData();
+    }, []);
+
+    const searchInPokemonList = (keyword) => {
+        setPokemonList(tempPokemonList);
+
+        const filterList = (list) => {
+            return list.filter(({ name }) => {
+               return name.includes(keyword);
+            });
+        }
+
+        setPokemonList(filterList);
+    };
+
+    const setCurrentPokemonID = async (pokemonID) => {
+        const data = await pokemonApi.getPokemonByID(pokemonID);
+        setCurrentPokemon(data);
+    };
+
+    return (
+        <PokemonContext.Provider value={{
+            pokemonList,
+            currentPokemon,
+            searchInPokemonList,
+            setCurrentPokemonID,
+        }}>
+            {children}
+        </PokemonContext.Provider>
+    )
+}
+
+PokemonProvider.propTypes = {
+    children: PropTypes.node,
+}
+
+const usePokemonContext = () => {
+    const context = useContext(PokemonContext);
+    if (context === undefined) {
+        throw new Error('usePokemonContext must be used within a PokemonProvider');
+    }
+
+    return context;
+}
+
+export { PokemonProvider, usePokemonContext };
