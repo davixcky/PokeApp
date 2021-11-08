59d70899078a05a764f7dbf15d52b3a7dde5f3b0
David Orozco <dorozco@convene.com>
2021-11-08 18:30:31 -0500

    feat: add pokemonDetails component

--- src/components/features/Home/Home.js
+++ src/components/features/Home/Home.js
@@ -3,14 +3,24 @@ import {usePokemonContext} from "../../../context/pokemonContext";
 import styles from './Home.module.css';
 import {PokemonSection} from "../../ui";
 import {PokemonPreview} from "../../ui/PokemonPreview";
+import {PokemonDetails} from "../../ui/PokemonDetails";
 
 const Home = () => {
     const [searchInput, setSearchInput] = useState('');
     const {pokemonList, searchInPokemonList} = usePokemonContext();
+    const [showModal, setShowModal] = useState(false);
 
     const onSearchPokemonChange = (e) => {
         setSearchInput(e.target.value);
     };
+
+    const onShowDetails = () => {
+        setShowModal(true);
+    };
+
+    const onCloseDetails = () => {
+        setShowModal(false);
+    }
 
     useEffect(() => {
         searchInPokemonList(searchInput);
@@ -20,7 +30,8 @@ const Home = () => {
         <div className={styles.container}>
             <input onInput={onSearchPokemonChange}/>
             <PokemonSection data={pokemonList}/>
-            <PokemonPreview/>
+            <PokemonPreview onShowDetails={onShowDetails}/>
+            { showModal && <PokemonDetails onClose={onCloseDetails}/>}
         </div>
     );
 }
---
+++ src/components/ui/PokemonDetails/PokemonDetails.js
@@ -0,0 +1,56 @@
+import React, {useEffect} from "react";
+import styles from './PokemonDetails.module.css';
+import PropTypes from "prop-types";
+import {Modal} from "../Modal";
+import {usePokemonContext} from "../../../context/pokemonContext";
+
+const PokemonDetails = ({onClose}) => {
+
+    const {currentPokemon, onCaptureCurrentPokemon} = usePokemonContext();
+
+    useEffect(() => {
+        console.log({currentPokemon});
+    }, []);
+
+    return (
+        <Modal onClose={onClose}>
+            <h1> {currentPokemon.name} </h1>
+            <img src={currentPokemon.mainImage} alt={currentPokemon.name} width={200} height={200}/>
+            <div className={styles.abilities}>
+                <h2>Abilities</h2>
+                <ul>
+                    {
+                        currentPokemon.abilities.map((ability) => {
+                            return (
+                                <li key={`ability-${ability}`}>
+                                    <p> {ability}</p>
+                                </li>
+                            );
+                        })
+                    }
+                </ul>
+            </div>
+            <div className={styles.abilities}>
+                <h2>Hidden abilities</h2>
+                <ul>
+                    {
+                        currentPokemon.hiddenAbilities.map((ability) => {
+                            return (
+                                <li key={`hidden-ability-${ability}`}>
+                                    <p> {ability}</p>
+                                </li>
+                            );
+                        })
+                    }
+                </ul>
+            </div>
+            <button onClick={onCaptureCurrentPokemon}>catch</button>
+        </Modal>
+    );
+};
+
+PokemonDetails.propTypes = {
+    onClose: PropTypes.func.isRequired,
+};
+
+export default PokemonDetails;
